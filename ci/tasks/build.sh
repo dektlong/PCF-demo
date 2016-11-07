44b33ab2a7ec06a42957922a298d9053168514e9
dektlong <dekel@vmware.com>
2016-11-06 17:07:42 -0800

    Update build.sh
    change to gradle

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,64 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -e
 
-inputDir=  outputDir=  versionFile=  artifactId=  packaging=
-
-while [ $# -gt 0 ]; do
-  case $1 in
-    -i | --input-dir )
-      inputDir=$2
-      shift
-      ;;
-    -o | --output-dir )
-      outputDir=$2
-      shift
-      ;;
-    -v | --version-file )
-      versionFile=$2
-      shift
-      ;;
-    -a | --artifactId )
-      artifactId=$2
-      shift
-      ;;
-    -p | --packaging )
-      packaging=$2
-      shift
-      ;;
-    * )
-      echo "Unrecognized option: $1" 1>&2
-      exit 1
-      ;;
-  esac
-  shift
-done
-
-error_and_exit() {
-  echo $1 >&2
-  exit 1
-}
-
-if [ ! -d "$inputDir" ]; then
-  error_and_exit "missing input directory: $inputDir"
-fi
-if [ ! -d "$outputDir" ]; then
-  error_and_exit "missing output directory: $outputDir"
-fi
-if [ ! -f "$versionFile" ]; then
-  error_and_exit "missing version file: $versionFile"
-fi
-if [ -z "$artifactId" ]; then
-  error_and_exit "missing artifactId!"
-fi
-if [ -z "$packaging" ]; then
-  error_and_exit "missing packaging!"
-fi
-
-version=`cat $versionFile`
-artifactName="${artifactId}-${version}.${packaging}"
-
-cd $inputDir
-./mvnw clean package -Pci -DversionNumber=$version
-
-# Copy war file to concourse output folder
-cd ..
-cp $inputDir/target/$artifactName $outputDir/$artifactName
+export GRADLE_OPTS=-Dorg.gradle.native=false
+version=`cat version/number`
+cd git-repo
+gradle assemble -Pversion=$version
+cp build/libs/*.war ../artifact-dir/
